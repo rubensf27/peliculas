2fa4726df50121f122ea0518827357f9081b602e
Rubén Pacheco <rubenpa.89@gmail.com>
2022-03-08 23:17:51 +0100

    Estructura principal de la app, añadiendo rutas principales y la Home y Detail Screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
+    return const Scaffold(
+      body: Center(
         child: Text('Detail Screen'),
       ),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
+    return const Scaffold(
+      body: Center(
         child: Text('Home Screen'),
       ),
     );
