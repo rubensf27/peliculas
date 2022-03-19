b84295d8271420b1da8917458ca2503b315e1fd9
Rubén Pacheco <rubenpa.89@gmail.com>
2022-03-19 09:22:51 +0100

    Commit finalizar detalles de diseño MovieSlider Horizontal

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -5,8 +5,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 280,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -17,6 +16,9 @@ class MovieSlider extends StatelessWidget {
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 )),
+          ),
+          const SizedBox(
+            height: 5,
           ),
           Expanded(
             child: ListView.builder(
@@ -39,11 +41,35 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.purple,
       margin: const EdgeInsets.symmetric(
         horizontal: 10,
         vertical: 10,
       ),
+      child: Column(children: [
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, 'details',
+              arguments: 'movie-instance'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(21),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/300x300/'),
+              width: 130,
+              height: 190,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Título fgjfdkjg fjdgj fdgnjkfdngkfjdngjkfdngjfdnjkgnf',
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+          textAlign: TextAlign.center,
+        ),
+      ]),
     );
   }
 }
