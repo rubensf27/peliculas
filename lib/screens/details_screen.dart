eb034a99e755ce6dea26c50880eae87fff065e2d
Rubén Pacheco <rubenpa.89@gmail.com>
2022-03-19 10:08:24 +0100

    Commit añadir slivers para añadir cierto comportamiento preprogramado al AppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,13 +1,53 @@
 import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
+import 'package:peliculas/themes/app_theme.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Detail Screen'),
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomSliverAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200,
+      pinned: true,
+      floating: false,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'Detalles',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ),
+        centerTitle: true,
+        background: const FadeInImage(
+          placeholder: AssetImage(
+            'assets/loading.gif',
+          ),
+          image: NetworkImage(
+            'https://via.placeholder.com/500x300',
+          ),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
